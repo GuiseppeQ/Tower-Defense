e0f2a00efa217db334aaa3dba2b3271afcbfb21d
Jack Skywalker <juandadim6@gmail.com>
2024-05-02 18:58:01 -0500

    Sistema De Oro
    se incrementa el oro con el paso tiempo, los botones hacen su respectiva resta de oro y genera el spawn.

---
+++ Tower defense/Assets/Scripts/Oro.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Oro : MonoBehaviour
+{
+    public float oroInicial = 0;
+    public float tasaIncrementoOro = 1f;
+    public GameObject tropaPrefab;
+    public Transform puntoDespliegueTropa;
+    public static float oroActual;
+
+
+
+    void Start()
+    {
+        oroActual = oroInicial;
+
+    }
+
+    void Update()
+    {
+        oroActual += tasaIncrementoOro * Time.deltaTime; // Incrementa el oro con el tiempo
+       
+    }
+
+    public void ComprarTropa(int costo)
+    {
+        if (oroActual >= costo)
+        {
+            oroActual -= costo; // Resta el costo de la tropa al oro actual
+            DesplegarTropa();
+        }
+        else
+        {
+            Debug.Log("¡No tienes suficiente oro!");
+        }
+    }
+
+    void DesplegarTropa()
+    {
+        Instantiate(tropaPrefab, puntoDespliegueTropa.position, Quaternion.identity);
+    }
+}
